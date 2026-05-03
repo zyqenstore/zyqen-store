a7a72e151e6dd9984182c7ca6ea68721e9ee54aa
zyqenstore <zyqenstore@gmail.com>
2026-05-03 09:59:15 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,9 +14,9 @@ const APP_SHELL = [
   BASE + "imagens/logo/logo.png",
   BASE + "imagens/logo/favicon.png",
 
-  BASE + "imagens/icons/icon1.png",
-  BASE + "imagens/icons/icon1.png",
+  BASE + "imagens/icons/icon11.png",
   BASE + "imagens/icons/icon12.png",
+  BASE + "imagens/icons/icon13.png",
 
   BASE + "imagens/icones/search.svg",
   BASE + "imagens/icones/flame.svg",
